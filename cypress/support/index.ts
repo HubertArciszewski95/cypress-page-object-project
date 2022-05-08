1710dca54f8309aa2075562ad020842e394a47a8
Hubert Arciszewski <hubertarciszewski@gmail.com>
2022-05-08 17:34:01 +0200

    Add mochawesome reporter

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,26 @@
 import './commands'
 
+import addContext from 'mochawesome/addContext'
 
+// Enable to add screenshots to the mochawesome report
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+        let item = runnable
+        const nameParts = [runnable.title]
+
+        while (item.parent) {
+            nameParts.unshift(item.parent.title)
+            // @ts-ignore
+            item = item.parent
+        }
+
+        const fullTestName = nameParts
+            .filter(Boolean)
+            .join(' -- ')
+
+        const imageUrl = `screenshots/${Cypress.spec.name
+            }/${fullTestName} (failed).png`
+        // @ts-ignore
+        addContext({ test }, imageUrl)
+    }
+})
